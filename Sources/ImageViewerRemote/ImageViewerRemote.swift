cd5a673ae41a03e73fe89ba35b70d940637a0cf3
Dmitry <11458173+anisim0v@users.noreply.github.com>
2020-09-22 14:06:11 +0300

    Update ImageViewerRemote.swift

--- Sources/ImageViewerRemote/ImageViewerRemote.swift
+++ Sources/ImageViewerRemote/ImageViewerRemote.swift
@@ -23,7 +23,7 @@ public struct ImageViewerRemote: View {
         _httpHeaders = State(initialValue: httpHeaders)
         _disableCache = State(initialValue: disableCache)
         self.aspectRatio = aspectRatio
-        if viewerShown{
+        if viewerShown == true{
             loader = ImageLoader(url: imageURL, httpHeaders: httpHeaders)
         }
     }
